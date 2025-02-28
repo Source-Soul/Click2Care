e077b9e0c71952ef18437e88af687d2b70e41fe4
Sadia Sultana <tithy404@gmail.com>
2025-02-28 19:08:08 +0600

    Patient login, registration, profile update
    Patient login and registration system is now functional. User/patient can update their profile information anytime

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import conneCloudinary from "./config/cloudnary.js";
 import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 //app config
 const app = express();
@@ -18,9 +19,8 @@ app.use(cors());
 
 //api endpoints
 app.use("/api/admin", adminRouter);
-//localhost:4000/api/admin/add-doctor
 app.use("/api/doctor", doctorRouter);
-//localhost:4000/api/doctor/list
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API WORKING GREAT");
