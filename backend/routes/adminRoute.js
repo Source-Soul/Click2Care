868d4460285e2ebb21f7677e66dc74378640b1d4
CherryJerry1 <jerinjalal22@gmail.com>
2025-03-07 15:05:19 +0600

    Dashboard Page for admin panel

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -3,7 +3,8 @@ import {
   addDoctor,
   allDoctors,
   loginAdmin, appointmentsAdmin,
-  appointmentCancel
+  appointmentCancel,
+  adminDashboard,
 } from "../controllers/adminController.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
@@ -17,4 +18,6 @@ adminRouter.post("/all-doctors", authAdmin, allDoctors);
 adminRouter.post("/change-availability", authAdmin, changeAvailability);
 adminRouter.get('/appointments',authAdmin,appointmentsAdmin)
 adminRouter.post('/cancel-appointment',authAdmin,appointmentCancel)
+adminRouter.get("/dashboard", authAdmin, adminDashboard);
+
 export default adminRouter;
