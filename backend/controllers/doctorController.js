36dcba1cfb9309a2d950e42ecd1d4b8f267675bd
Aurin <kundu.prionty246@gmail.com>
2025-03-08 00:26:28 +0600

    Api created for doctors appointment

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,3 +1,4 @@
+import appointmentModel from "../models/appointmentModel.js";
 import doctorModel from "../models/doctorModel.js";
 
 const changeAvailability = async (req, res) => {
@@ -23,4 +24,19 @@ const doctorList = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
-export { changeAvailability, doctorList };
+
+//API TO GET DOCTOR APPOINTMENT FOR DOCTOR PANEL
+const appointmentsDoctor = async(req,res)=>{
+  try {
+    const { docId }= req.body 
+    const appointments = await appointmentModel.find({ docId })
+    res.json({ success: true, appointments})  
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+    
+  }
+}
+
+
+export { changeAvailability, doctorList , appointmentsDoctor};
