ddef8ead1197824b7565542fc9973c0398da182b
Sadia Sultana <tithy404@gmail.com>
2025-03-07 21:39:31 +0600

    Doctor dashboard part2

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -4,10 +4,12 @@ import { toast } from "react-toastify";
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  const [aToken, setAToken] = useState(localStorage.getItem("aToken") || "");
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
   const [doctors, setDoctors] = useState([]);
   const [appointments, setAppointments] = useState([]);
-  const [dashData, setDashData]= useState(false)
+  const [dashData, setDashData] = useState(false);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -79,22 +81,21 @@ const AdminContextProvider = (props) => {
       toast.error(error.message);
     }
   };
-  const getDashData = async()=>{
-    try{
-      const{data}=await axios.get(backendUrl+ '/api/admin/dashboard',{headers:{aToken}})
-      if(data.success){
-        setDashData(data.dashData)
+  const getDashData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/admin/dashboard", {
+        headers: { aToken },
+      });
+      if (data.success) {
+        setDashData(data.dashData);
         console.log(data.dashData);
-        
-
-      }else{
-        toast.error(data.message)
+      } else {
+        toast.error(data.message);
       }
-    }
-    catch(error){
+    } catch (error) {
       toast.error(error.message);
     }
-  }
+  };
 
   const value = {
     aToken,
@@ -107,7 +108,8 @@ const AdminContextProvider = (props) => {
     setAppointments,
     getAllAppointments,
     cancelAppointment,
-    dashData, getDashData,
+    dashData,
+    getDashData,
   };
   return (
     <AdminContext.Provider value={value}>
---
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DoctorAppointments = () => {
+  return (
+    <div>
+      <h1>Doctor Appointments</h1>
+    </div>
+  );
+};
+export default DoctorAppointments;
